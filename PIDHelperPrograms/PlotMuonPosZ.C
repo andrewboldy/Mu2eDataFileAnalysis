2db115d5257c91fc775b5474abaa6b013d9c5f61
Andrew Boldy <aboldy@mu2egpvm01.fnal.gov>
2025-05-14 15:18:34 -0500

    Adding the test pdfs from Andy's code to determine how they look

---
+++ PIDHelperPrograms/PlotMuonPosZ.C
@@ -0,0 +1,49 @@
+//
+//PlotMuonPosZ.C 
+//Written by Andy, Adapted by Andrew Boldy 
+//Summer 2025
+// An example of how to plot the initial z-position of MCParticle muons
+// This uses cut functions defined in common_cuts.hh
+//
+
+#include "EventNtuple/utils/rooutil/inc/RooUtil.hh"
+#include "EventNtuple/utils/rooutil/inc/common_cuts.hh"
+
+#include "TH1F.h"
+#include "TCanvas.h"
+
+void PlotMuonPosZ(std::string filename) {
+
+  // Create the histogram you want to fill
+  TH1F* hMuonPosZ = new TH1F("hMuonPosZ", "", 6000,4000,10000);
+  TCanvas* c1 = new TCanvas("c1","c1");
+  // Set up RooUtil
+  RooUtil util(filename);
+  //  util.Debug(true);
+  // Loop through the events
+  for (int i_event = 0; i_event < util.GetNEvents(); ++i_event) {
+    // Get the next event
+    auto& event = util.GetEvent(i_event);
+
+    // Get the e_minus tracks from the event
+    auto e_minus_tracks = event.GetTracks(is_e_minus);
+
+    // Loop through the e_minus tracks
+    for (auto& track : e_minus_tracks) {
+
+      // Get the track segments at the tracker entrance and has an MC step
+      auto mu_particles = track.GetMCParticles(is_muon);
+
+      // Loop through the tracker entrance track segments
+      for (auto& particle : mu_particles) {
+
+        // Fill the histogram
+        hMuonPosZ->Fill(particle.mcsim->pos.z());
+      }
+    }
+  }
+
+  // Draw the histogram
+  hMuonPosZ->Draw("HIST E");
+  c1->SaveAs("multiFileHistograms/eMinusHists/posHists/PlotMuonPosZ.pdf");
+}
